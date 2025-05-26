b77b4547f935a30677e8c7716a280e299b71d7d4
Tran Minh Khanh <kidopro03@gmail.com>
2025-05-26 15:50:44 +0700

    ProductNotFoundException Exception

---
+++ shoppingcart/src/main/java/com/shopdream/shoppingcart/exceptions/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.shopdream.shoppingcart.exceptions;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
